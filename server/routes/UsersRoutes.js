64f8c0b2ce78d9ccc57c88bda68c382960525100
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-02-08 23:57:35 -0600

    Añadir ruta de usuario

---
+++ server/routes/UsersRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express();
+
+// CRUD USUARIOS
+// Create - POST
+router.post('/users');
+
+// Read - GET
+router.get('/users');
+router.get('/users/:id');
+
+// Update - PUT/PATCH
+router.put('/users/:id');
+router.patch('/users/:id');
+
+// Delete - DELETE
+router.delete('/users/:id');
+
+module.exports = router;
