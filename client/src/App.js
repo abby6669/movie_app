19062ff01d4b4cc34f22021664cdabd83e6b6cea
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-02-09 00:35:00 -0600

    Visualizar componentes en app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Card from './components/Card';
 import Login from './components/Login';
-import Navegacion from './components/Navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { MovieGrid } from './components/MovieGrid';
 
@@ -10,28 +10,16 @@ function App() {
   return (
 
     <>
+    <Navbar />
     <div>
       <header>
         <h1>Hola desde Movie App! 🍿</h1>
       </header>
     </div>
     <Card/>
-        <Navegacion />
-        <br></br>
-        <div>
-          <h1>Movies</h1>
-          <MovieGrid />
-        </div>
-        <br></br>
-      </header>
-
-      <footer>
-
-        <Footer />
-        
-      </footer>
-    </div>
+    <MovieGrid />
     <Login/>
+    <Footer />
     </>
 
   );
