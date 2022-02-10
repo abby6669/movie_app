8af601b398ac906ddbb109a3680b714429801b4e
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-02-10 00:00:17 -0600

    Crear vista de registro e instalacion de formik

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Profile from './components/Profile';
-import Carousel from './components/Carousel';
+import Register from './views/Register'
 
 function App() {
   return (
 
     <>
-    <Navbar />
-    <Carousel />
-    {/* <div>
-      <header>
-        <h1>Hola desde Movie App! 🍿</h1>
-      </header>
-    </div>   */}
-    <Footer />
-    {/* <Profile /> */}
+    <Register />
     </>
   );
 }
