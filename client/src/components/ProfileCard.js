bb76f1d819b876c06a3edb0201354f8403a02b11
Abby6669 <abbylittman@gmail.com>
2022-02-09 21:33:26 -0600

    Profile card y profile view

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,14 +1,19 @@
-import {Card, Form, Button} from 'react-bootstrap';
+import {Card, Img, Button} from 'react-bootstrap';
+import Foto from "./Perfil.jpeg";
+
 
 export function ProfileCard() {
     return (
 
         <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="#" />
+  <Card.Img variant="top" src={Foto} alt="foto perfil" />
   <Card.Body>
-    <Card.Title>Nombre</Card.Title>
+    <Card.Title>John Miller</Card.Title>
     <Card.Text>
-      Datos de Usuario.
+      <ul>
+          <li>miller@gmail.com</li>
+          <li>Comedia</li>
+      </ul>
     </Card.Text>
     <Button variant="primary">Actualizar Perfil</Button>
   </Card.Body>
