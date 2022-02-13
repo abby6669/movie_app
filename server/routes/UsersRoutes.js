01eb5a0de59e6fb52130dfa4abb322da430f251f
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-02-12 20:44:49 -0600

    Crear rutas usuario, likes, comentarios y watched

--- server/routes/UsersRoutes.js
+++ server/routes/UsersRoutes.js
@@ -1,18 +1,47 @@
 const express = require('express');
-const router = express();
+const router = express.Router();
+const { UsersController, AuthController, LikesController, CommentsController, WatchedController } = require('../controllers')
 
 // CRUD USUARIOS
 // Create - POST
-router.post('/users');
+// router.post('/users');
 
 // Read - GET
-router.get('/users');
-router.get('/users/:id');
+router.get('/users', UsersController.getAllUsers);
+// router.get('/users/:id');
+
+// AUTH Usuarios
+// Register - POST
+router.post('/auth/register', AuthController.createUser);
 
 // Update - PUT/PATCH
-router.put('/users/:id');
+router.put('/auth/:id', AuthController.updateUser);
 
-// Delete - DELETE
-router.delete('/users/:id');
+// Login - POST
+router.post('/auth/login', AuthController.loginUser);
+
+// Logout - POST
+router.post('/auth/logout', AuthController.logoutUser);
+
+// URLS Likes 
+//Create like - POST
+router.post('/like', LikesController.createLike);
+
+// Delete Like - DELETE
+router.delete('/like/:id', LikesController.deleteLike);
+
+// URLS Comments 
+//Create like - POST
+router.post('/comment', CommentsController.createComment);
+
+// Delete Like - DELETE
+router.delete('/comment/:id', CommentsController.deleteComment);
+
+// URLS Watched 
+//Create like - POST
+router.post('/watched', WatchedController.createWatched);
+
+// Delete Like - DELETE
+router.delete('/watched/:id', WatchedController.deleteWatched);
 
 module.exports = router;
