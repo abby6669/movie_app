b8fe2529ccd0d287297393cd07e6c06cb638a73d
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-02-12 20:43:53 -0600

    Crear index y server e instalar mongoose y dotenv

--- server/index.js
+++ server/index.js
@@ -1,20 +1,7 @@
-//MODULES
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const app = express();
+const app = require('./server');
+const PORT = 8080;
 
-//MIDDLEWARES
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev'))
-app.use(helmet());
-
-//ENDPOINTS
-// app.use('/api/v1', require('./routes'));
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-    console.log("max!!");
-})
-
-module.exports = app;  
+//LISTENER
+app.listen(PORT, () => 
+console.log(`Servidor corriendo: http://localhost:${PORT}`)
+);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
-const PORT = 8080;
+//MODULES
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const routes = require('./routes');
+const app = express();
 
-//LISTENER
-app.listen(PORT, () => 
-console.log(`Servidor corriendo: http://localhost:${PORT}`)
-);
+//MIDDLEWARES
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+app.use(helmet());
+
+//ENDPOINTS
+app.use('/api/v1', require('./routes'));
+// app.get('/', (req, res) => {
+//     res.status(200).send('Hello World!');
+//     console.log("max!!");
+// })
+
+module.exports = app;  
