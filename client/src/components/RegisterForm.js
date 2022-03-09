8b61d9c343b049dc5caf61445c81289d7d9069b5
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-03-08 23:17:09 -0600

    Poner en funcionamiento el componente register

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,28 +1,26 @@
-import {Form, Button, Card} from 'react-bootstrap/';
+import {Form, Button, Card, Alert} from 'react-bootstrap/';
 import { Link, useNavigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
-import {useAuth} from '../contexts/AuthContext'
-//import axios from 'axios'
+import { useAuth } from '../contexts/AuthContext'
 
 function RegisterForm() {
-    // const {register} = useAuth()
-    // function handleSubmit() {
-    //     const headers = {'Authorization': `Bearer ${token}`}
-    //     const URL_API_LIKES = 'http://localhost:8080/api/v1/users/register'
-    //     return axios.post(URL_API_LIKES, {email: 'email', password: '123'}, {headers})
-    //     .then(response => console.log(response.data))
-    //     .catch(error => error.message);
-    // }
+      const [ error, setError ] = useState('');
+      const [ loading, setLoading ] = useState(false);
+
       const emailRef = useRef();
       const passwordRef = useRef();
-    //  const confirmpasswordRef = useRef();
+      const confirmPasswordRef = useRef();
+
       const { currentUser, register } = useAuth();
-      const [ error, setError ] = useState('');
-      const [ loading, setLoading ] = useState('');
       const navigate = useNavigate();
 
       async function handleSubmit(e){
         e.preventDefault();
+
+        const passwordsMatch = passwordRef.current.value === confirmPasswordRef.current.value;
+        if(!passwordsMatch) {
+          return setError('Passwords no coinciden!');
+        }
 
         try {
           setError('')
@@ -45,24 +43,24 @@ function RegisterForm() {
       <Card className="w-75 mx-auto my-5">
           <Card.Body>
               <h1 className="display-4 text-center my-3">Registrarse</h1>
-              <Form >
-              {/* onSubmit={ handleSubmit } */}
+              { error && error !== '' && <Alert variant="danger">{ error }</Alert> }
+              <Form onSubmit={ handleSubmit }>
                   <Form.Group className="mb-3" controlId="formEmail">
                       <Form.Label>Email</Form.Label>
-                      <Form.Control type="email" placeholder="Ingresa tu email" autoComplete="off" required />
+                      <Form.Control ref={emailRef} type="email" placeholder="Ingresa tu email" autoComplete="off" required />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formPassword">
                       <Form.Label>Contraseña</Form.Label>
-                      <Form.Control type="password" placeholder="Ingresa tu contraseña" autoComplete="off" required />
+                      <Form.Control ref={passwordRef} type="password" placeholder="Ingresa tu contraseña" autoComplete="off" required />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formPassword">
                       <Form.Label> Confirmación de contraseña </Form.Label>
-                      <Form.Control type="password" placeholder="Confirma tu contraseña" autoComplete="off" required />
+                      <Form.Control ref={confirmPasswordRef} type="password" placeholder="Confirma tu contraseña" autoComplete="off" required />
                   </Form.Group>
 
-                  <Button className="w-100" variant="primary" type="submit">
+                  <Button className="w-100" variant="primary" type="submit" disabled={ loading }>
                       Crear una nueva cuenta
                   </Button>
               </Form>
