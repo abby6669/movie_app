11f814490f921ecb6b91152e695f01b3be7b2339
Max H <78988911+MaxNaudek@users.noreply.github.com>
2022-03-14 18:59:53 -0600

    Agrego forma hardcodeada de renderizar los comments usando Navigate .

--- client/src/components/PostComment.js
+++ client/src/components/PostComment.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 import { useRef, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { useParams } from 'react-router-dom'
-import {useAuth} from '../contexts/AuthContext'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
+
 
 function CommentCard(props) {
   const { currentUser, token } = useAuth();
   const [loading, setLoading] = useState(false)
   const commentRef = useRef();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   function handleComment(e) {
     e.preventDefault();
@@ -16,7 +18,7 @@ function CommentCard(props) {
     const headers = {'Authorization': `Bearer ${token}`}
     const URL_COMMENT = 'http://localhost:8080/api/v1/comments'
     const newCommentBody = {
-        itemId: id, 
+        itemId: id,
         userId: currentUser.id,
         content: commentRef.current.value
     }
@@ -24,17 +26,18 @@ function CommentCard(props) {
     return axios
     .post(URL_COMMENT, newCommentBody, {headers})
     .then(() => setLoading(false))
-    .catch(error => error.message);  
+    .then(navigate(`/movies/${id}`+1-1))
+    .catch(error => error.message);
 }
 
   return (
-      <> 
-        <h3 className="display-5 my-5 ms-5">Deja tu comentario:</h3>     
+      <>
+        <h3 className="display-5 my-5 ms-5">Deja tu comentario:</h3>
         <Form className="ms-5 w-50 mb-5" onSubmit={handleComment}>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Control ref={commentRef} as="textarea" rows={3} />
           </Form.Group>
-          <Button type="submit" disabled={loading}>Enviar comentario</Button>
+          <Button type="submit"  disabled={loading}>Enviar comentario</Button>
         </Form>
     </>
   )
